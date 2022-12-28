71449697128946ab81ad2db45e4d19f83388deb3
faustinopsy <rodrigohipnose@gmail.com>
2022-12-27 22:28:08 -0300

    iniciando estudos LaraVue

---
+++ resources/js/Layouts/Authenticated.vue
@@ -0,0 +1,10 @@
+<!-- Navigation Links -->
+<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
+    <BreezeNavLink :href="route('dashboard')" 
+    :active="route().current('dashboard')">Dashboard</BreezeNavLink>
+  
+    <BreezeNavLink :href="route('posts.index')" 
+    :active="route().current('posts.index')">Posts</BreezeNavLink>
+  
+  </div>
